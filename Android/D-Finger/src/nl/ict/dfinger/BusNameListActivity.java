c1bb783577ac61f446c35fa0969be50eca60f467
Benny <Benny@gmx.it>
2013-01-26 14:49:33 +0100

    Removed .classpath from gitignore added D-finger project

---
+++ Android/D-Finger/src/nl/ict/dfinger/BusNameListActivity.java
@@ -0,0 +1,14 @@
+package nl.ict.dfinger;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class BusNameListActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
