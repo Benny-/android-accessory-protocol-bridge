c31c9b56eaf49f50cca5ad4bfbe66975d9f9f4b4
Benny Jacobs <Benny@GMX.it>
2013-02-04 12:36:12 +0100

    MISC: converted tabs to spaces

--- Accessory/AndroidAccessory/doc/example.c
+++ Accessory/AndroidAccessory/doc/example.c
@@ -16,48 +16,48 @@ int main (int argc, char *argv[])
     
     const char* const uuids[] = {
         /* You should generate your own uuid using uuidgen, dont copy paste this */
-		"db0d662d-40cf-4995-9849-6dc604563e10",
-		NULL
-	};
-	
-	accessory = initAccessory(
-		"Echo server company",
-		"Echo server",
-		"This echo server will send anything back you send it",
-		uuids,
-		"1.0",
-		// I diddent actually test if blueterm can communicate to libAndroidAccessory
-		"https://play.google.com/store/apps/details?id=es.pymasde.blueterm",
-		"5890699");
-	
-	printf("Waiting for new connection\n");
-	con = getNextAndroidConnection(accessory);
-	printf("New connection accepted\n");
-	while(connected)
-	{
-	    int read;
-	    char buffer[1024];
-	    
-	    read = readAccessory(con, buffer, sizeof(buffer));
-	    if(read < 0)
-	    {
-	        connected = 0;
-	        break;
-	    }
-	    
-	    printf("Read %i bytes\n", read);
-	    
-	    // Note: I use writeAllAccessory() instead of writeAccessory() here.
-	    // This function guarantees to write everything or return a error.
-	    if(writeAllAccessory(con, buffer, read))
-	    {
-	        connected = 0;
-	        break;
-	    }
-	}
-	printf("Connection ended.\n");
-	
-	closeAndroidConnection(con);
-	deInitaccessory(accessory);
+        "db0d662d-40cf-4995-9849-6dc604563e10",
+        NULL
+    };
+    
+    accessory = initAccessory(
+        "Echo server company",
+        "Echo server",
+        "This echo server will send anything back you send it",
+        uuids,
+        "1.0",
+        // I diddent actually test if blueterm can communicate to libAndroidAccessory
+        "https://play.google.com/store/apps/details?id=es.pymasde.blueterm",
+        "5890699");
+    
+    printf("Waiting for new connection\n");
+    con = getNextAndroidConnection(accessory);
+    printf("New connection accepted\n");
+    while(connected)
+    {
+        int read;
+        char buffer[1024];
+        
+        read = readAccessory(con, buffer, sizeof(buffer));
+        if(read < 0)
+        {
+            connected = 0;
+            break;
+        }
+        
+        printf("Read %i bytes\n", read);
+        
+        // Note: I use writeAllAccessory() instead of writeAccessory() here.
+        // This function guarantees to write everything or return a error.
+        if(writeAllAccessory(con, buffer, read))
+        {
+            connected = 0;
+            break;
+        }
+    }
+    printf("Connection ended.\n");
+    
+    closeAndroidConnection(con);
+    deInitaccessory(accessory);
 }
 
