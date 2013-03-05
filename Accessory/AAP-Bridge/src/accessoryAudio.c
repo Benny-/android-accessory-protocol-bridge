542dbb7979ad214684eb4be0c993f6099d8c83d4
Benny Jacobs <Benny@GMX.it>
2013-03-05 13:23:34 +0100

    Added product id's for all audio accessory devices

--- Accessory/AAP-Bridge/src/accessoryAudio.c
+++ Accessory/AAP-Bridge/src/accessoryAudio.c
@@ -20,7 +20,6 @@ static void on_new_source(pa_context *c, const pa_source_info *i, int eol, void 
 	printf("pa_source_info: %p eol: %i\n", i, eol);
 	if(!eol && i->index > lastIndexChecked)
 	{
-		printf("%s",pa_proplist_to_string(i->proplist));
 		if(pa_proplist_contains(i->proplist,"device.vendor.id") && pa_proplist_contains(i->proplist,"device.product.id"))
 		{
 			printf("This might be a usb accessory audio source. Lets check it.\n");
@@ -28,7 +27,7 @@ static void on_new_source(pa_context *c, const pa_source_info *i, int eol, void 
 			const char* vendor_id = pa_proplist_gets(i->proplist,"device.vendor.id");
 			const char* product_id = pa_proplist_gets(i->proplist,"device.product.id");
 
-			if(!strcmp(vendor_id, "18d1") && !strcmp(product_id, "2d03"))
+			if(!strcmp(vendor_id, "18d1") && (!strcmp(product_id, "2d02") || !strcmp(product_id, "2d03") || !strcmp(product_id, "2d05")) )
 			{
 				char argument[20];
 				sprintf(argument, "source=%i", i->index);
